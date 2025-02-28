acdf76e05fcb1438faf90e706e6974743da81efb
uzuw <uzuwxd@gmail.com>
2025-02-28 05:05:47 +0000

    modifying homepage

--- musiclibrary/frontend/src/pages/Home.tsx
+++ musiclibrary/frontend/src/pages/Home.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Homepage
+    <>
+    <div className='w-full h-1/4 bg-gray-200'>
+      <img src='' alt="header image"/>
+    </div> 
+    <div className='p-10 flex flex-col items-center justify-center'>
+      <h1 className='p-3 text-4xl font-sans'>SONATA </h1>
+      <h3 className='p-3 text-2xl font-sans'>The album rating website </h3>
     </div>
+    </>
   )
 }
 
